0a0d25d0ca2e5e2f3b3e5e98bcbf6a46f11d71b7
Inan Xu <ixu2002new@gmail.com>
2023-08-31 23:14:58 -0700

    update test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const meElement = screen.getByText(/Inan Xu/i);
+  expect(meElement).toBeInTheDocument();
 });
